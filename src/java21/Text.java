41ff37e3eb54de664b7259ad07339b3ce23ee7cc
LKQ0303 <1752828349@qq.com>
2020-06-28 11:41:57 +0800

    第二次提交

---
+++ src/java21/Text.java
@@ -0,0 +1,27 @@
+package java21;
+
+
+public class Text extends Thread {
+
+@Override
+	public void run() {
+		//TODO Auto-generated method stub
+		// super.run();
+		for (int i=0;i<2000;i++) {
+			System.out.println(i+ "+++多线程+++");
+		}
+	}
+
+	public static void main(String[] args) {
+		Text thread =new Text();
+		thread.start();
+		// TODO Auto-generated method stub
+for (int i=0; i<=2000; i++) {
+	System.out.println(i + "---主线方程---");
+}
+
+}
+}
+
+
+
