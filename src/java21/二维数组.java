82e5bad95f70c2b57cd49bcdd3d78444bdfb92b5
LKQ0303 <1752828349@qq.com>
2020-07-29 17:55:52 +0800

    第四次提交

---
+++ src/java21/二维数组.java
@@ -0,0 +1,16 @@
+package java21;
+public class ถþฮฌสýื้ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        int data[][] =new int[][] {
+		{5,7,9},{12,14,16,18},{23,25,36,47},{22,54,65,15},{22,34}};
+		for(int x=0;x<data.length;x++){
+		for(int y=0;y<data[x].length;y++){
+
+		        System.out.print(data[x][y]+"\t");
+		    }
+		System.out.println();
+		}
+		}
+}
